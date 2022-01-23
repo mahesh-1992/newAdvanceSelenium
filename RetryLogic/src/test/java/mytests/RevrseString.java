7311700fac4ca94752330fac6a961f7127280dc5
MAHESH KUMAR <MAHESH KUMAR@DESKTOP-KTPP931>
2022-01-23 15:29:51 +0530

    adding java code for reverse a string

---
+++ RetryLogic/src/test/java/mytests/RevrseString.java
@@ -0,0 +1,17 @@
+package mytests;
+
+public class RevrseString {
+
+	public static void main(String[] args) 
+	{
+		String s="india";
+		String rev="";
+		for(int i=s.length()-1;i>=0;i--)
+		{
+			rev=rev+s.charAt(i);
+		}
+		System.out.println(rev);
+
+	}
+
+}
